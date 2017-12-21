70f53b7c71eb4baa2b06b551d98d57e6802bd923
alexrt96 <alexrt96@gmail.com>
2017-12-21 03:41:39 +0100

    Eliminando cargar

--- app/src/main/java/com/example/dgp/conejonegro/Configuracion.java
+++ app/src/main/java/com/example/dgp/conejonegro/Configuracion.java
@@ -8,6 +8,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -23,82 +25,33 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 
-public class Configuracion extends AppCompatActivity {
+public class Configuracion extends AppCompatActivity implements Runnable{
 
     private String idioma = " ";
     private Spinner spinner;
     ConexionBD conexion = null;
     private ProgressDialog progressDialog;
+    private int tipoHebra;
+    ArrayList<String> idiomas;
+    ArrayAdapter<String> adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        progressDialog= new ProgressDialog(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.configuracion);
 
         actualizarPantalla();
 
         spinner = (Spinner) findViewById(R.id.spIdioma);
-        ArrayList<String> idiomas = new ArrayList<String>();
+        idiomas = new ArrayList<String>();
 
-        try {
-            conexion = new ConexionBD();
-            ResultSet rs = conexion.hacerConsulta("SELECT * FROM IDIOMA");
-            while(rs.next()){
-                idiomas.add(rs.getString("nombre"));
-            }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (java.sql.SQLException e) {
-            e.printStackTrace();
-        }
+        progressDialog = ProgressDialog.show(this, "Cargando", "Descargando idiomas", true,
+                false);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, idiomas);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                idioma = parent.getItemAtPosition(position).toString();
-                try {
-                    actualizarIdioma(conexion);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-                actualizarPantalla();
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        comprobarConfiguracion();
-
-        Button btn = (Button)findViewById(R.id.salasconfiguracionBoton);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Guardar en la clase de contenidos persistentes
-                crearConfiguracion();
-
-
-                startActivity(new Intent(Configuracion.this, Principal.class));
-                try {
-                    conexion.cerrarBasedeDatos();
-                } catch (java.sql.SQLException e) {
-                    e.printStackTrace();
-                }
-                finish();
-            }
-        });
+        tipoHebra = 1;
+        Thread thread = new Thread(this);
+        thread.start();
     }
 
     private void crearConfiguracion(){
@@ -132,7 +85,7 @@ public class Configuracion extends AppCompatActivity {
         }
     }
 
-    public void actualizarIdioma(ConexionBD conexion) throws SQLException, ClassNotFoundException {
+    public void actualizarIdioma() throws SQLException, ClassNotFoundException {
 
 
         SharedPreferences config=getSharedPreferences("traducciones", Context.MODE_PRIVATE);
@@ -167,7 +120,86 @@ public class Configuracion extends AppCompatActivity {
         TextView mTextView5 = (TextView)findViewById(R.id.configuracionTextoSonido);
         mTextView5.setText(config.getString("configuracionTextoSonido", "Activar sonido"));
 
-        Button button = (Button)findViewById(R.id.salasconfiguracionBoton);
+        Button button = (Button)findViewById(R.id.configuracionBoton);
         button.setText(config.getString("configuracionBoton", "VOLVER"));
     }
+
+
+    @Override
+    public void run() {
+        switch(tipoHebra){
+            case 1:
+                try {
+                    conexion = new ConexionBD();
+                    ResultSet rs = conexion.hacerConsulta("SELECT * FROM IDIOMA");
+                    while(rs.next()){
+                        idiomas.add(rs.getString("nombre"));
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (java.sql.SQLException e) {
+                    e.printStackTrace();
+                }
+        }
+        handler.sendEmptyMessage(0);
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            despuesDeCargar();
+            progressDialog.dismiss();
+        }
+    };
+
+    private void despuesDeCargar(){
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, idiomas);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                idioma = parent.getItemAtPosition(position).toString();
+
+                try {
+                    actualizarIdioma();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                actualizarPantalla();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        comprobarConfiguracion();
+
+        Button btn = (Button)findViewById(R.id.configuracionBoton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Guardar en la clase de contenidos persistentes
+                crearConfiguracion();
+
+                startActivity(new Intent(Configuracion.this, Principal.class));
+                try {
+                    conexion.cerrarBasedeDatos();
+                } catch (java.sql.SQLException e) {
+                    e.printStackTrace();
+                }
+                finish();
+            }
+        });
+    }
+
 }
